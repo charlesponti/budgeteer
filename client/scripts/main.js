65e82449a252b5eddbe241e02a055ff0125d96e4
CJ Ponti <cjponti@gmail.com>
2014-10-15 17:50:43 +0100

    Rename socket initial connection event to 'born'

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -4,13 +4,11 @@ window.$ = require('jquery');
 window.Backbone = require('backbone');
 Backbone.$ = $;
 
-var messages = [];
 var socket = io.connect('http://localhost:4000');
 
-socket.on('message', function (data) {
+socket.on('born', function (data) {
   console.log(data);
 });
-
 
 var AppModel = require("./app/app");
 window.App = new AppModel();
