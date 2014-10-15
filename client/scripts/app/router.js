c3ccc0a7dca8a460a86c1037a092927f30d5d363
CJ Ponti <cjponti@gmail.com>
2014-10-15 23:33:21 +0100

    Add loadTaskApp

--- client/scripts/app/router.js
+++ client/scripts/app/router.js
@@ -1,12 +1,19 @@
 "use strict";
 
+var TaskMainView = require('./view/tasks/main');
+
 var AppRouter = Backbone.Router.extend({
   
   routes: {
-    'account': 'onAccount',
+    'tasks': 'loadTaskApp',
 
     // Default - catch all
     '*actions': 'defaultAction'
+  },
+
+  loadTaskApp: function() {
+    var view = new TaskMainView();
+    view.render();
   }
 
 });
