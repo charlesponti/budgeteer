83d9b9451c5189ba4368dc0b9b5ec3f751381d2a
CJ Ponti <cjponti@gmail.com>
2014-10-15 11:17:42 +0100

    Add test file for Task model

---
+++ test/models/task.js
@@ -0,0 +1,21 @@
+'use strict';
+
+require('../spec_helper');
+
+var chai = require('chai');
+var expect = chai.expect;
+var Task = require('../../server/models/task');
+
+describe('Task model', function() {
+
+  var task;
+
+  beforeEach(function() {
+    task = new Task();
+  });
+
+  afterEach(function() {
+    task = null;
+  });
+
+})
