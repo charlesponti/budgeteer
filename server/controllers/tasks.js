1df90658fe70b05e99bbe8c03c211b36c5770cf8
CJ Ponti <cjponti@gmail.com>
2014-10-19 22:26:14 +0100

    Refactor TaskController using react/lib/merge

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -1,14 +1,11 @@
 'use strict';
 
 var express = require('express');
-var Emitter = require('events').EventEmitter;
+var events = require('events');
 var Task = require('../models/task');
+var merge = require('react/lib/merge');
 
-function TaskController() {
-
-  var self = this;
-
-  self.emitter = new Emitter();
+var TaskController = merge(events.EventEmitter.prototype, {
 
   /**
    * Retrieve current user's tasks and attach them to req.user.tasks
@@ -16,12 +13,12 @@ function TaskController() {
    * @param  {ServerResonse}   res
    * @param  {Function} next
    */
-  this.getUserTasks = function(req, res, next) {
+  getUserTasks: function(req, res, next) {
     if (req.isAuthenticated()) {
       req.user.tasks = Task.find({ user_id: req.user.id });
     }
     next();
-  };
+  },
 
   /**
    * Retrieve either all user's tasks or a specific set of tasks if tasks query
@@ -30,7 +27,7 @@ function TaskController() {
    * @param  {ServerResponse}   res
    * @param  {Function} next
    */
-  this.index = function(req, res, next) {
+  index: function(req, res, next) {
     var tasks;
     if (req.query.id) {
       tasks = req.user.tasks.where({ id: { $in: req.params.id }});
@@ -39,60 +36,71 @@ function TaskController() {
     }
     tasks.exec(function(err, tasks) {
       if (err) {
-        return res.status(500).json({ 
-          message: 'There was an issue fetching your tasks' 
+        return res.status(500).json({
+          message: 'There was an issue fetching your tasks'
         });
       }
       res.status(200).json({ tasks: tasks });
     });
-  };
+  },
 
-  this.create = function(req, res, next) {
+  create: function(req, res, next) {
     req.user.tasks
       .where({ title: req.body.title })
       .exec(function(err, tasks) {
-        self.emitter.emit('create-find', err, tasks, req, res, next);
+        TaskController.emit('create-find', err, tasks, req, res, next);
       });
-  };
+  },
 
-  this.update = function(req, res, next) {
+  update: function(req, res, next) {
 
-  };
+  },
 
-  this.destroy = function(req, res, next) {
+  destroy: function(req, res, next) {
+    req.user.tasks
+      .where({ id: req.body.id })
+      .exec(function(err, tasks) {
+        TaskController.emit('after-delete', err, tasks, req, res, next);
+      });
+  }
+});
 
-  };
-
-  self.emitter.on('create-find', function(err, tasks, req, res, next) {
+TaskController.on('create-find', function(err, tasks, req, res, next) {
+  if (err) {
+    return res.status(500).json({
+      message: 'There was an issue creating your tasks'
+    });
+  }
+  var task = new Task();
+  task.title = req.body.title;
+  task.description = req.body.description;
+  task.user_id = req.user.id;
+  task.save(function(err, task) {
     if (err) {
-      return res.status(500).json({ 
-        message: 'There was an issue creating your tasks' 
+      return res.status(500).json({
+        message: 'There was an issue creating your task',
+        raw: err
       });
     }
-    var task = new Task();
-    task.title = req.body.title;
-    task.description = req.body.description;
-    task.user_id = req.user.id;
-    task.save(function(err, task) {
-      if (err) {
-        return res.status(500).json({ 
-          message: 'There was an issue creating your task',
-          raw: err
-        });
-      }
-      res.status(201).json({ task: task });
-    });
+    res.status(201).json({ task: task });
   });
+});
 
-  var router = express.Router();
+TaskController.on('after-delete', function(err, tasks, req, res, next) {
+  if (err) {
+    console.log(err);
+  }
+  res.flash('success', 'Task deleted');
+  res.status(200).json({ message: 'Task Deleted', task: req.body.id });
+});
 
-  router.get('/', Cthulhu.securePath, this.getUserTasks, this.index);
-  router.post('/', Cthulhu.securePath, this.getUserTasks, this.create);
-  router.put('/', Cthulhu.securePath, this.getUserTasks, this.update);
-  router.delete('/', Cthulhu.securePath, this.getUserTasks, this.destroy);
+var router = express.Router();
 
-  this.router = router;
-  
-}
+router.get('/', Cthulhu.securePath, TaskController.getUserTasks, TaskController.index);
+router.post('/', Cthulhu.securePath, TaskController.getUserTasks, TaskController.create);
+router.put('/', Cthulhu.securePath, TaskController.getUserTasks, TaskController.update);
+router.delete('/', Cthulhu.securePath, TaskController.getUserTasks, TaskController.destroy);
 
-module.exports = new TaskController();
+TaskController.router = router;
+
+module.exports = TaskController;
