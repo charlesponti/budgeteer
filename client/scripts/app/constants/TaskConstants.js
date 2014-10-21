482339066daaee41bb60457d1c261ed5fc081d48
CJ Ponti <cjponti@gmail.com>
2014-10-21 22:23:38 +0100

    Add additional task constants

--- client/scripts/app/constants/TaskConstants.js
+++ client/scripts/app/constants/TaskConstants.js
@@ -4,8 +4,18 @@ module.exports = {
 
   CREATE: 'task-create',
 
+  CREATED: 'task-created',
+
   UPDATE: 'task-update',
 
-  DESTROY: 'task-destroy'
+  UPDATED: 'task-updated',
+
+  DESTROY: 'task-destroy',
+
+  DESTROYED: 'task-destroyed',
+
+  EDIT: 'task-edit',
+
+  EDITED: 'task-edited'
 
 };
