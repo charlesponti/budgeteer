f3f7b64b2c52d128c66e64530af2969d80265f3d
CJ Ponti <cjponti@gmail.com>
2014-10-21 12:07:59 +0100

    Add html to render function

--- client/scripts/app/components/account/ConnectedAccount.jsx
+++ client/scripts/app/components/account/ConnectedAccount.jsx
@@ -4,6 +4,8 @@
  * Module dependencies
  */
 var React = require('react');
+var LinkButton = require('./LinkButton.jsx');
+var UnlinkButton = require('./UnlinkButton.jsx');
 
 /**
  * Component containing all of a user's connected accounts
@@ -20,9 +22,25 @@ var ConnectedAccount = React.createClass({
    * @return {object}
    */
   render: function() {
+    var provider = this.props.provider;
+    var profile = this.props.profile.profile;
+
     return (
       <li className="list-group-item">
-        <h4>{this.props.account.profile.email}</h4>
+        <h4> {this.props.provider} </h4>
+        <div class="row">
+          <div class="col-sm-4">
+            <p>
+              <b> Name: </b> {profile.name}
+            </p>
+            <p>
+              <b> Screenname: </b> {profile.screen_name}
+            </p>
+          </div>
+          <div class="col-sm-4">
+            <UnlinkButton provider={provider}/>
+          </div>
+        </div>
       </li>
     );
   }
