92d575115ef436e32ba31bc06cad9d7492b7b7e4
CJ Ponti <cjponti@gmail.com>
2014-10-27 18:01:27 +0000

    Add TaskRouter.spec.js

---
+++ test/routers/TaskRouter.spec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('TaskRouter', function() {
+
+  var TaskRouter, req, res;
+
+  beforeEach(function() {
+    // req = HttpFixtures.req;
+    // res = HttpFixtures.res;
+  });
+
+  afterEach(function() {
+
+  });
+
+  describe('.afterUpdate()', function() {
+    it('should', function() {
+      // var fn = TaskRouter.afterUpdate();
+    });
+  });
+
+});
