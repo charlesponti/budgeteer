8f5ccac88415d3c8c6b464b2c2f3eae266000f2b
CJ Ponti <cjponti@gmail.com>
2014-11-02 15:06:49 +0000

    Refactor app/main.jsx with documentation

--- client/app/main.jsx
+++ client/app/main.jsx
@@ -1,15 +1,28 @@
 'use strict';
 
+// Connect to WebSocket
 var io = window.io;
-window._ = require('lodash');
-window.Backbone = require('backbone');
-Backbone.$ = window.$;
-var Layout = require('./components/Layout.jsx');
 var socket = io.connect('http://localhost:4000');
-
-var AppRouter = require('./router.jsx');
-AppRouter.start();
-
 socket.on('born', function (data) {
   console.log(data);
 });
+
+// Module dependencies
+var React = require('react');
+
+// Create factory for Modal
+var Modal = React.createFactory(require('./components/Modal.jsx'));
+
+// Attach lodash to window
+window._ = require('lodash');
+
+// Attach Backbone to window
+window.Backbone = require('backbone');
+Backbone.$ = window.$;
+
+// Requery application router and start it
+var AppRouter = require('./router.jsx');
+AppRouter.start();
+
+// Render modal
+React.render(<Modal/>, document.getElementById('modal'));
