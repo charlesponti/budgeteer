15f59d5dfcdc930f5a5a74058d1ecf31643468b2
CJ Ponti <cjponti@gmail.com>
2014-11-03 21:03:17 +0000

    Add edit and delete to preview

--- client/app/components/tasks/Preview.jsx
+++ client/app/components/tasks/Preview.jsx
@@ -3,6 +3,9 @@
 var React = require('react');
 var marked = require('marked');
 
+var TaskActions = require('../../actions/TaskActions');
+var TaskForm = React.createFactory(require('./Form.jsx'));
+
 /**
  * Preview component
  * @type {ReactCompositeComponent}
@@ -13,13 +16,35 @@ var Preview = React.createClass({
     text: React.PropTypes.string.isRequired
   },
 
+  onEditClick: function(e, id) {
+    TaskActions.loadTaskForm({
+      title: 'Edit Task',
+      data: this.props.task,
+      component: <TaskForm task={this.props.task}/>
+    });
+  },
+
+  onDeleteClick: function(e, id) {
+    TaskActions.deleteTask(this.props.task);
+  },
+
   /**
    * @returns {ReactCompositeComponent}
    */
   render: function() {
-    var rawMarkup = marked(this.props.text);
+    var rawMarkup = marked(this.props.task.description);
     return (
-      <div className="preview" dangerouslySetInnerHTML={{__html: rawMarkup}}/>
+      <div>
+        <div className="preview" dangerouslySetInnerHTML={{__html: rawMarkup}}/>
+        <div className="pull-right">
+          <button onClick={this.onEditClick} className="btn btn-default"> 
+            Edit Task 
+          </button>
+          <button onClick={this.onDeleteClick} className="btn btn-danger"> 
+            Delete Task 
+          </button>
+        </div>
+      </div>
     );
   }
 
