2da59a4f56544b4cb8a71a07a6dba2da164cf18f
CJ Ponti <cjponti@gmail.com>
2014-11-10 12:57:29 +0000

    Create TaskApp component

---
+++ client/app/components/TaskApp.jsx
@@ -0,0 +1,27 @@
+'use strict';
+
+// Module dependencies
+var React = require('react');
+
+var TabPanel = require('../mixins/TabPanel.jsx');
+
+var TaskMain = require('./tasks/Main.jsx');
+var CategoryMain = require('./categories/Main.jsx');
+
+var TaskApp = React.createClass({
+  
+  mixins: [TabPanel], 
+  
+  getDefaultProps: function() {
+    return {
+      tabs: {
+        'Tasks': <TaskMain />,
+        'Categories': <CategoryMain />
+      },
+      default: 'Tasks'
+    };
+  }
+
+});
+
+module.exports = TaskApp;
