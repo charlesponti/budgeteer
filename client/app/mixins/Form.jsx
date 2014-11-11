c2c69a4a3a787a6bebb06d3d8793f2c320197a09
CJ Ponti <cjponti@gmail.com>
2014-11-11 16:02:02 +0000

    Add makeFields and makeField to FormMixin

--- client/app/mixins/Form.jsx
+++ client/app/mixins/Form.jsx
@@ -5,6 +5,38 @@ var React = require('react');
 var FormMixin = {
 
   /**
+   * Construct field
+   * @param  {string} type Type of form input
+   * @param  {string} name Name of form input
+   * @return {ReactElement}
+   */
+  makeField: function(type, name) {
+    switch(type) {
+      case 'hidden':
+        return <input type='hidden' name={name}>;
+      case 'input':
+        return <input type='hidden' name={name} className="form-control">;
+      case 'textarea':
+        return <textarea className="form-control" name={name}></textarea>;
+    }
+  },
+
+  /**
+   * Construct fields
+   * @return {ReactElement[]}
+   */
+  makeFields: function() {
+    this.fields.map(function(field) {
+      if (this.type instanceof String) {
+        return this.makeField(field.type, field.name)  
+      }
+      if (this.type instanceof ReactComponent) {
+        return field;
+      }
+    }.bind(this))
+  },
+
+  /**
    * Make form-group
    * @param  {string} name  Name of input field
    * @param  {string} label Text for label element
