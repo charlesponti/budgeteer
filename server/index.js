81f2e737b98456cc489d8328db9ea71c1e93aa1f
CJ Ponti <cjponti@gmail.com>
2014-12-06 22:44:02 +0000

    Update server setup

--- server/index.js
+++ server/index.js
@@ -1,29 +1,30 @@
 'use strict';
 
 /**
- * Current Node environment
- * @private
- */
-var env = process.env.NODE_ENV;
-
-/**
  * Module dependencies.
  * @private
  */
-var _ = require('lodash');
 var cthulhu = require('cthulhu');
 var auth = require('cthulhu-auth');
-var util = require('util');
-var http = require('http');
-var swig = require('swig');
-var path = require('path');
-var amqp = require('amqp');
 var mongoose = require('mongoose');
 var favicon = require('serve-favicon');
+
+/**
+* Application dependencies.
+* @private
+*/
 var config = require('./config');
-
 var User = require('./models/user');
+var oauth = config.OAuth;
 
+// Set oauth strategies
+auth.use('Facebook', oauth.Facebook);
+auth.use('Google', oauth.Google);
+auth.use('Twitter', oauth.Twitter);
+auth.use('Foursquare', oauth.Foursquare);
+auth.use('Github', oauth.Github);
+
+// Create application
 var app = cthulhu({
   port: 4000,
   public: './public',
@@ -33,21 +34,18 @@ var app = cthulhu({
   sessionStore: config.sessionStore
 });
 
+// Use favicon
 app.use(favicon(__dirname + '/../public/favicon.ico'));
 
+// Initialize cthulhu-auth
 app.use(auth.initialize());
 
-/**
- * Deserialize user from session
- */
+// Deserialize user from session
 app.use(auth.deserializeUser(function(user, done) {
-  console.log(user);
   User.findOne(user._id).exec(done);
 }));
 
-/**
- * Connect to database
- */
+// Connect to database
 app.startDB = function() {
   var db = config.DB;
   mongoose.connect(db, function(err) {
@@ -59,18 +57,8 @@ app.startDB = function() {
   });
 };
 
-var oauth = config.OAuth;
-
-app.oauth = {
-  Facebook: auth.Facebook(oauth.Facebook),
-  Google: auth.Google(oauth.Google),
-  Twitter: auth.Twitter(oauth.Twitter),
-  Foursquare: auth.Foursquare(oauth.Foursquare),
-  Github: auth.Github(oauth.Github)
-};
-
-GLOBAL.Cthulhu = app;
-
+// Add routes to app
 app.use(require('./routers'));
 
+// Export app
 module.exports = app;
