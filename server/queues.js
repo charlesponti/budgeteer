7a7e25582d321f37186ec448a59f9683badc8e1c
CJ Ponti <cjponti@gmail.com>
2014-12-07 11:32:29 +0000

    Refactor queues

--- server/queues.js
+++ server/queues.js
@@ -1,73 +1,80 @@
 'use strict';
 
-// Connect to RabbitMQ
-app.rabbitConnection = amqp.createConnection({
-  host: 'localhost'
-});
+var amqp = require('amqp');
+var util = require('util');
 
-/**
-* Application's RabbitMQ queues
-* @type {object}
-*/
-app.queues = {};
+exports.setup = function(app) {
 
-/**
-* Make new RabbitMQ message queue
-* @param {string} name Name of queue
-*/
-app.addQueue = function(name) {
-  var msg;
+  // Connect to RabbitMQ
+  app.rabbitmq = amqp.createConnection({
+    host: 'localhost'
+  });
 
-  if (app.queues[name]) {
-    msg = name+' queue already exists';
+  /**
+   * Application's RabbitMQ queues
+   * @type {object}
+   */
+  app.queues = {};
+
+  /**
+   * Make new RabbitMQ message queue
+   * @param {string} name Name of queue
+   */
+  app.addQueue = function(name) {
+    var msg;
+
+    if (app.queues[name]) {
+      msg = name+' queue already exists';
+      util.log(msg);
+    }
+
+    app.queues[name] = app.rabbitmq.queue(name);
+    msg = name+' queue is ready for use';
     util.log(msg);
-  }
+  };
 
-  app.queues[name] = app.rabbitConnection.queue(name);
-  msg = name+' queue is ready for use';
-  util.log(msg);
-};
+  /**
+   * Retrive RabbitMQ queue
+   * @param {string} name Name of queue
+   */
+  app.getQueue = function(name) {
+    return app.queues[name];
+  };
 
-/**
-* Retrive RabbitMQ queue
-* @param {string} name Name of queue
-*/
-app.getQueue = function(name) {
-  return app.queues[name];
-};
+  /**
+   * Application's RabbitMQ exchanges
+   * @type {object}
+   */
+  app.exchanges = {};
 
-/**
-* Application's RabbitMQ exchanges
-* @type {object}
-*/
-app.exchanges = {};
+  /**
+   * Make new RabbitMQ message exchange
+   * @param {string} name Name of exchange
+   */
+  app.addExchange = function(name) {
+    var msg;
 
-/**
-* Make new RabbitMQ message exchange
-* @param {string} name Name of exchange
-*/
-app.addExchange = function(name) {
-  var msg;
+    if (app.exchanges[name]) {
+      msg = name+' exchange already exists';
+      util.log(msg);
+    }
 
-  if (app.exchanges[name]) {
-    msg = name+' exchange already exists';
+    app.exchanges[name] = app.rabbitmq.exchange(name);
+    msg = name+' exchange is ready for use';
     util.log(msg);
-  }
+  };
 
-  app.exchanges[name] = app.rabbitConnection.exchange(name);
-  msg = name+' exchange is ready for use';
-  util.log(msg);
+  /**
+   * Retrive RabbitMQ queue
+   * @param {string} name Name of queue
+   */
+  app.getExchange = function(name) {
+    return app.exchanges[name];
+  };
+
+  // Log message when connected to RabbitMQ
+  app.rabbitmq.on('ready', function() {
+    app.logger.info('RabbitMQ connected.');
+  });
+
 };
-
-/**
-* Retrive RabbitMQ queue
-* @param {string} name Name of queue
-*/
-app.getExchange = function(name) {
-  return app.exchanges[name];
-};
-
-// Log message when connected to RabbitMQ
-app.rabbitConnection.on('ready', function() {
-  app.logger.info('RabbitMQ connected.');
-});
