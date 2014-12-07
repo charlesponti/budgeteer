299668d24a5d94278c1da4a9c87f5d4341e9f6cf
CJ Ponti <cjponti@gmail.com>
2014-12-07 15:03:55 +0000

    Fix require in spec_helper.js

--- test/spec_helper.js
+++ test/spec_helper.js
@@ -2,7 +2,7 @@
 /**
  * Assign GLOBAL values to be used in tests
  */
-GLOBAL.Cthulhu = require('../index');
+GLOBAL.Cthulhu = require('../server/index');
 GLOBAL.HttpFixtures = require('./fixtures/http');
 var User = GLOBAL.User = require('../server/models/user');
 GLOBAL.user = new User();
