61343165319c853a142d0d31c0160027976ef4a5
CJ Ponti <cjponti@gmail.com>
2014-12-11 18:03:37 +0000

    Add karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function(config) {
+  config.set({
+
+    browsers: [
+      'Chrome'
+    ],
+
+    frameworks: [
+      'jasmine'
+    ],
+
+    files: [
+      'client/test/**/*spec.js'
+    ],
+
+    preprocessors: {
+      'client/test/**/*spec.js': ['webpack']
+    },
+
+    webpack: {
+      debug: true,
+      resolve: {
+        modulesDirectories: ['bower_components', 'node_modules']
+      }
+    },
+
+    webpackServer: {
+      noInfo: true
+    },
+
+    plugins: [
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      require("karma-webpack")
+    ]
+
+  });
+};
