08a8b52ab3cdedf7611de8ad0132d55005670888
CJ Ponti <cjponti@gmail.com>
2014-12-12 20:24:24 +0000

    Update task store tests

--- client/test/specs/stores/TaskStore-spec.js
+++ client/test/specs/stores/TaskStore-spec.js
@@ -1,73 +1,27 @@
-'use strict';
-
-var base = '../../../app/';
-var service = require(base+'service/api');
-var TaskStore = require(base+'stores/TaskStore');
-var AppConstants = require(base+'constants/App');
-
 describe('TaskStore', function() {
+  'use strict';
 
   var store, task, promise;
+  var $ = require('jquery');
+  var service = require('../../../app/service/api');
+  var TaskStore = require('../../../app/stores/TaskStore');
+  var AppConstants = require('../../../app/constants/App');
 
   beforeEach(function() {
-    store = TaskStore;
     promise = new Promise(function() {});
+    store = TaskStore;
     task = {
       title: 'foo',
       description: 'bar',
       completed: false,
       category: 'default'
     };
-    promise.resolve = Promise.resolve.bind(promise, {
-      tasks: [
-        { _id: 1, title: 'foo' }
-      ]
-    });
-    promise.reject = Promise.reject.bind(promise, { message: 'Error' });
-    spyOn(promise, 'then').andReturn(promise);
-    spyOn(promise, 'catch');
+    spyOn($, 'ajax');
   });
 
   afterEach(function() {
     task =
-    store =
-    promise = undefined;
-  });
-
-  describe('.load()', function() {
-    it('should call service.get', function() {
-      spyOn(service, 'get').andReturn(promise);
-      store.load();
-      expect(service.get).toHaveBeenCalledWith('tasks');
-      expect(promise.then).toHaveBeenCalled();
-    });
-  });
-
-  describe('.create()', function() {
-    it('should call service.post', function() {
-      spyOn(service, 'post').andReturn(promise);
-      store.create(task);
-      expect(service.post).toHaveBeenCalledWith('tasks', task);
-      expect(promise.then).toHaveBeenCalled();
-    });
-  });
-
-  describe('.udpate()', function() {
-    it('should call service.put', function() {
-      spyOn(service, 'put').andReturn(promise);
-      store.update(task);
-      expect(service.put).toHaveBeenCalledWith('tasks', task);
-      expect(promise.then).toHaveBeenCalled();
-    });
-  });
-
-  describe('.destroy()', function() {
-    it('should call service.del', function() {
-      spyOn(service, 'del').andReturn(promise);
-      store.destroy(task);
-      expect(service.del).toHaveBeenCalledWith('tasks', task);
-      expect(promise.then).toHaveBeenCalled();
-    });
+    store = undefined;
   });
 
   describe('.dispatcherIndex()', function() {
