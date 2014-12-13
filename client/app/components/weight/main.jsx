25f793a05f92aae6cb6160bd53e6783ffe37a819
CJ Ponti <cjponti@gmail.com>
2014-12-13 17:22:29 +0000

    Add weight/main.jsx

---
+++ client/app/components/weight/main.jsx
@@ -0,0 +1,22 @@
+'use strict';
+
+var React = require('react');
+
+var WeightMain = React.createClass({
+
+  /**
+   * Render component
+   * @return {ReactElement}
+   */
+  render: function() {
+    return (
+      <div class="row">
+        <h1> Weight </h1>
+        <h2> Coming Soon... </h2>
+      </div>
+    );
+  }
+
+});
+
+module.exports = WeightMain;
