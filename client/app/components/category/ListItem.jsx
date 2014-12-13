208e11c65f7cfcd879e2628000f5f69fadda91ac
CJ Ponti <cjponti@gmail.com>
2014-12-13 16:51:44 +0000

    Update style declaration of category list item

--- client/app/components/category/ListItem.jsx
+++ client/app/components/category/ListItem.jsx
@@ -18,16 +18,13 @@ var ListItem = React.createClass({
   render: function() {
     var record = this.props.record;
 
-    var style = {
-      color: record.color,
-      display: 'inline-block',
-      marginRight: '1em',
-      fontSize: '3em'
+    var iconColor = {
+      color: record.color
     };
 
     return (
       <li className="list-group-item">
-          <i className="fa fa-tag fa-4 category-icon" style={style}></i>
+          <i className="fa fa-tag fa-4 category-icon" style={iconColor}></i>
           <h4>{record.name}</h4>
       </li>
     );
