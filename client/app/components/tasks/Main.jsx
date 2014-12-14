657c8d05e29c19a8267bf387dc83d688060e338e
CJ Ponti <cjponti@gmail.com>
2014-12-14 14:29:39 +0000

    Update tasks/Main.jsx

--- client/app/components/tasks/Main.jsx
+++ client/app/components/tasks/Main.jsx
@@ -8,45 +8,34 @@ var AppActions = require('../../actions/app');
 var TaskStore = require('../../stores/TaskStore');
 
 // Components
-var TaskForm = React.createFactory(require('./Form.jsx'));
-var TaskList = React.createFactory(require('./List.jsx'));
+var TaskList = require('./List.jsx');
+var TaskAddButton = require('./add-button.jsx');
 
 /**
  * Main view for the Tasks application
- * @type {ReactElement}
+ * @type {ReactCompositeComponent}
  */
 var TaskMain = React.createClass({
 
   displayName: 'TaskMain',
 
   /**
-   * Perform actions when component will get mounted to the DOM
+   * Load TaskStore
    */
   componentWillMount: function() {
     TaskStore.load();
   },
 
-  onAddClick: function() {
-    AppActions.loadModal({
-      title: 'Create Task',
-      component: TaskForm({task: {}})
-    });
-  },
-
   /**
    * Render view
+   * @return {ReactElement}
    */
   render: function() {
     return (
-      <div className="row">
-        <h1>
-          Tasks
-          <button onClick={this.onAddClick}
-            className="btn btn-default pull-right">
-            Add Task
-          </button>
-        </h1>
+      <div className="row tasks">
+        <br/>
         <TaskList className="task-list" id='task-list' />
+        <TaskAddButton/>
       </div>
     );
   }
