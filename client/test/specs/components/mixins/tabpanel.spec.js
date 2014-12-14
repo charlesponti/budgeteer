bd4b9eff61d6f5925369e73b447b52fed85ed3d9
CJ Ponti <cjponti@gmail.com>
2014-12-14 14:31:47 +0000

    Add tabpanel.spec.js

---
+++ client/test/specs/components/mixins/tabpanel.spec.js
@@ -0,0 +1,33 @@
+describe('Mixins: tab-panel', function() {
+  'use strict';
+
+  var tabPanel;
+  var React = require('react/addons');
+  var TestUtils = React.addons.TestUtils;
+  var TabPanel = require('../../../../app/mixins/TabPanel.jsx');
+
+  beforeEach(function() {
+    tabPanel = TabPanel;
+  });
+
+  afterEach(function() {
+    tabPanel = undefined;
+  });
+
+  describe('.getInitialState()', function() {
+
+  });
+
+  describe('.onClick()', function() {
+
+  });
+
+  describe('.render()', function() {
+
+  });
+
+  describe('.constructTabs()', function() {
+
+  });
+
+});
