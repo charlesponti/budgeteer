7a40c66d2b89525604721f78c48c735e69590dfa
CJ Ponti <cjponti@gmail.com>
2014-12-14 14:28:31 +0000

    Remove React.createFactory

--- client/app/components/category/Main.jsx
+++ client/app/components/category/Main.jsx
@@ -8,8 +8,8 @@ var AppActions = require('../../actions/app');
 var CategoryStore = require('../../stores/CategoryStore');
 
 // Components
-var CategoryForm = React.createFactory(require('./Form.jsx'));
-var CategoryList = React.createFactory(require('./List.jsx'));
+var CategoryForm = require('./Form.jsx');
+var CategoryList = require('./List.jsx');
 
 /**
  * Main view for the Tasks application
@@ -40,14 +40,14 @@ var CategoryMain = React.createClass({
     return (
       <div className="row">
 
-        <h1> 
+        <h1>
           Categories
-          <button onClick={this.onAddClick} 
+          <button onClick={this.onAddClick}
             className="btn btn-default pull-right">
             Add Category
           </button>
         </h1>
-        
+
         <CategoryList className="task-list" id='task-list' />
 
       </div>
