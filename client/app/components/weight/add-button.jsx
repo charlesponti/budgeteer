d560abcce0097808a4802bafde0c8f94458856a4
CJ Ponti <cjponti@gmail.com>
2014-12-15 10:31:54 +0000

    Add WeightAddButton

---
+++ client/app/components/weight/add-button.jsx
@@ -0,0 +1,27 @@
+'use strict';
+
+var React = require('react');
+
+var AppActions = require('../../actions/app');
+var WeightForm = require('./form.jsx');
+
+var WeightAddButton = React.createClass({
+
+  onClick: function() {
+    AppActions.loadModal({
+      title: 'Add Weight',
+      component: <WeightForm weight={{}} />
+    });
+  },
+
+  render: function() {
+    return (
+      <button onClick={this.onClick} className="btn add btn-default">
+      +
+      </button>
+    );
+  }
+
+});
+
+module.exports = WeightAddButton;
