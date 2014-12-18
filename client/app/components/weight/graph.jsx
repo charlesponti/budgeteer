0090e9edd43f4177522b1206c7c993c323c27bb3
CJ Ponti <cjponti@gmail.com>
2014-12-18 23:13:12 +0000

    Use react-chartist for weight chart

--- client/app/components/weight/graph.jsx
+++ client/app/components/weight/graph.jsx
@@ -1,39 +1,30 @@
 'use strict';
 
 var React = require('react');
+var Chartist = require('react-chartist');
+var WeightStore = require('../../stores/weight');
 
 var WeightGraph = React.createClass({
 
-  /**
-   * Create initial state for component
-   * @param {object}
-   */
   getInitialState: function() {
     return {
-      labels: [
-        "January", "February", "March", "April", "May", "June", "July"
-      ],
-      datasets: [
-        {
-          label: "My First dataset",
-          fillColor: "rgba(220,220,220,0.2)",
-          strokeColor: "rgba(220,220,220,1)",
-          pointColor: "rgba(220,220,220,1)",
-          pointStrokeColor: "#fff",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-          data: [65, 59, 80, 81, 56, 55, 40]
-        }
-      ]
+      labels: [],
+      series: []
     };
   },
 
+  onWeightStoreUpdate: function() {
+    return this.setState({
+      labels: WeightStore.getDates(),
+      series: [WeightStore.getWeights()]
+    });
+  },
+
   /**
-   * Create new Chart
+   * Listen for changes on WeightStore and update chart
    */
-  componentDidMount: function() {
-    var ctx = document.getElementById("myChart").getContext("2d");
-    new Chart(ctx).Line(this.state);
+  componentWillMount: function() {
+    return WeightStore.on('add', this.onWeightStoreUpdate);
   },
 
   /**
@@ -42,7 +33,9 @@ var WeightGraph = React.createClass({
    */
   render: function() {
     return (
-      <canvas id="myChart" width="400" height="400"></canvas>
+      <div>
+        <Chartist data={this.state} type={'Line'} />
+      </div>
     );
   }
 
