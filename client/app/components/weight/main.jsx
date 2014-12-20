45275bcb9622189e93f73dc04666dcd3717509cf
CJ Ponti <cjponti@gmail.com>
2014-12-20 12:33:14 +0000

    Manage weight props from WeightMain

--- client/app/components/weight/main.jsx
+++ client/app/components/weight/main.jsx
@@ -4,13 +4,27 @@ var React = require('react');
 
 var WeightStore = require('../../stores/weight');
 
+var WeightTable = require('./table.jsx');
 var WeightGraph = require('./graph.jsx');
 var WeightAddButton = require('./add-button.jsx');
 
 var WeightMain = React.createClass({
 
+  getInitialState: function() {
+    return {
+      weights: []
+    };
+  },
+
+  onWeightStoreChange: function() {
+    this.setState({
+      weights: WeightStore.models
+    });
+  },
+
   componentWillMount: function() {
     WeightStore.fetch();
+    WeightStore.on('change reset add', this.onWeightStoreChange);
   },
 
   /**
@@ -19,9 +33,10 @@ var WeightMain = React.createClass({
    */
   render: function() {
     return (
-      <div class="row">
+      <div className="row">
         <h1> Weight </h1>
-        <WeightGraph/>
+        <WeightGraph weights={this.state.weights}/>
+        <WeightTable weights={this.state.weights}/>
         <WeightAddButton/>
       </div>
     );
