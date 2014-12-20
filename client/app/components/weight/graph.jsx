8e091f4d09790fde8b15a402703ff8ffa4fd88e1
CJ Ponti <cjponti@gmail.com>
2014-12-20 13:36:02 +0000

    Use HighCharts for WeightGraph

--- client/app/components/weight/graph.jsx
+++ client/app/components/weight/graph.jsx
@@ -1,35 +1,50 @@
 'use strict';
 
-var google = window.google;
 var React = require('react');
+var Highcharts = window.Highcharts;
 var WeightStore = require('../../stores/weight');
 
 var WeightGraph = React.createClass({
 
-  getInitialState: function() {
-    return {
-      labels: [],
-      series: []
-    };
-  },
-
   drawChart: function() {
-    var i = 0;
     var weights = this.props.weights;
-    var data = new google.visualization.DataTable();
-    data.addColumn('date', 'Date');
-    data.addColumn('number', 'Weight');
-    data.addRows(weights.length);
-    weights.forEach(function(weight) {
-      data.setCell(i, 0, new Date(weight.get('date')));
-      data.setCell(i, 1, weight.get('kgs'));
-      return i++;
+    new Highcharts.Chart({
+      title: {
+        text: 'Weight',
+        align: 'left',
+        x: 90,
+        y: 30,
+        floating: true
+      },
+      chart: {
+        renderTo: 'weight-chart'
+      },
+      xAxis: {
+        categories: weights.map(function(weight) {
+          return weight.getDate().toLocaleDateString();
+        })
+      },
+      yAxis: {
+        title: {
+          text: 'Kilograms'
+        }
+      },
+      tooltip: {
+        valueSuffix: 'kgs'
+      },
+      series: [
+        {
+          name: 'Weight',
+          data: weights.map(function(weight) {
+            return weight.get('kgs');
+          })
+        }
+      ]
     });
-    return this.chart.draw(data);
   },
 
   componentDidMount: function() {
-    this.chart = new google.visualization.LineChart(this.getDOMNode());
+    return this.drawChart(this.props.weights);
   },
 
   componentDidUpdate: function() {
