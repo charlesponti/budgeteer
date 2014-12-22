0417758695ce8fe98e8f1fd976c207b63c27eaff
CJ Ponti <cjponti@gmail.com>
2014-12-22 09:53:59 +0000

    Add tests for tasks main

---
+++ client/test/specs/components/tasks/main.spec.js
@@ -0,0 +1,41 @@
+describe('Components: TasksMain', function() {
+  'use strict';
+
+  var component,
+    TestUtils = require('react/addons').addons.TestUtils,
+    TaskStore = require('../../../../app/stores/tasks'),
+    TaskMain = require('../../../../app/components/tasks/main.jsx');
+
+  beforeEach(function() {
+    spyOn(TaskStore, 'fetch');
+    component = TestUtils.renderIntoDocument(TaskMain());
+  });
+
+  afterEach(function() {
+    component = undefined;
+  });
+
+  describe('.getInitialState()', function() {
+    it('should have initial state', function() {
+      expect(component.state.tasks).toEqual([]);
+    });
+  });
+
+  describe('.onTaskStoreChange()', function() {
+    beforeEach(function() {
+      spyOn(component, 'setState');
+      spyOn(component, 'isMounted');
+    });
+    it('should not call .setState() if not mounted', function() {
+      component.isMounted.and.returnValue(false);
+      component.onTaskStoreChange();
+      expect(component.setState).not.toHaveBeenCalled();
+    });
+    it('should call .setState() if mounted', function() {
+      component.isMounted.and.returnValue(true);
+      component.onTaskStoreChange();
+      expect(component.setState).toHaveBeenCalled();
+    });
+  });
+
+});
