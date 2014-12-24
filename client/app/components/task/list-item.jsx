a3474e3c52107eb1371fa4320034edf81255740e
CJ Ponti <cjponti@gmail.com>
2014-12-24 14:14:02 +0000

    Refactor HTML of task/list-item

--- client/app/components/task/list-item.jsx
+++ client/app/components/task/list-item.jsx
@@ -59,14 +59,12 @@ var TaskListItem = React.createClass({
       <li className="list-group-item task-list-item" key={task.get('_id')}>
         <div>
           <input type="checkbox" className="task-checkbox"
-            onClick={this.onCheckboxClick} defaultChecked={task.completed}/>
+            onClick={this.onCheckboxClick} defaultChecked={task.get('completed')}/>
 
           <h4 onClick={this.onTitleClick}>{task.get('title')}</h4>
         </div>
-        <div>
           <i className="fa fa-tag fa-4" style={categoryStyle}></i>
           {category.name}
-        </div>
       </li>
     );
   }
