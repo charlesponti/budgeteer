e2d57fb9249fa6f09c616881aab18cf2c9b29ce2
CJ Ponti <cjponti@gmail.com>
2014-12-24 10:51:48 +0000

    Add router for /tasks

---
+++ server/routers/tasks.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var React = require('react/addons');
+var router = require('express').Router();
+var Task = require('mongoose').model('Task');
+var TaskView = require('../../client/app/components/TaskApp');
+
+router.get('/', function(req, res, next) {
+  return Task
+    .find({ user_id: req.user._id })
+    .exec(function(err, tasks) {
+      if (err) {
+        return next(err);
+      }
+      return res.render('layout', {
+        current_user: req.user,
+        view: React.renderToString(TaskView({
+          initialData: tasks
+        }))
+      });
+    });
+});
+
+module.exports = router;
