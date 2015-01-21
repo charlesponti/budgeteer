028426e38153c7d6518b53c0f1c3a3967f9f10aa
CJ Ponti <cjponti@gmail.com>
2015-01-21 13:39:49 +0000

    Remove unused AppDispatcher functions

--- client/app/dispatchers/app.js
+++ client/app/dispatchers/app.js
@@ -5,26 +5,4 @@ var Dispatcher = require('flux').Dispatcher;
 // Create instance of Dispatcher
 var AppDispatcher = new Dispatcher();
 
-/**
- * Dispatch event for api actions
- * @param {String|Object} action
- */
-AppDispatcher.handleAPIAction = function(action) {
-  this.dispatch({
-    source: 'API_ACTION',
-    action: action
-  });
-};
-
-/**
- * Dispatch event for view actions
- * @param {String|Object} action
- */
-AppDispatcher.handleViewAction = function(action) {
-  this.dispatch({
-    source: 'VIEW_ACTION',
-    action: action
-  });
-};
-
 module.exports = AppDispatcher;
