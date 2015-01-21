627b5eaa97384c3e2dbba566568b9dbffe60d057
CJ Ponti <cjponti@gmail.com>
2015-01-21 13:55:14 +0000

    Update development.js

--- server/config/environments/development.js
+++ server/config/environments/development.js
@@ -1,4 +1,7 @@
-
+/**
+ * @desc Configuration for applicatio
+ * @type {Object}
+ */
 module.exports = {
 
   "port": 3000,
@@ -17,58 +20,13 @@ module.exports = {
 
   "views": "./views",
 
-  "baseUrl": "http://localhost:4000",
-
-  "facebook": {
-    "app_id": "foobar",
-    "app_secret": "foobar",
-    "callback_url": "foobar"
-  },
-
-  "twitter": {
-    "token": "foobar",
-    "token_secret": "foobar",
-    "consumer_key": "foobar",
-    "consumer_secret": "foobar",
-    "callback_url": "foobar"
-  },
+  "baseUrl": "http://127.0.0.1:3000",
 
   "google": {
     "realm": "http://127.0.0.1:3000",
     "client_id": process.env.GOOGLE_ID,
     "client_secret": process.env.GOOGLE_SECRET,
     "redirect_uri": "http://127.0.0.1:3000/auth/google/callback"
-  },
-
-  "foursquare": {
-    "client_id": "foobar",
-    "client_secret": "foobar",
-    "callback_url": "foobar"
-  },
-
-  "github": {
-    "client_id": "foobar",
-    "client_secret": "foobar",
-    "token": "foobar",
-    "callback_url": "foobar"
-  },
-
-  "authy": {
-    "url": "foobar",
-    "sandbox_url": "foobar",
-    "api_key": "foobar",
-    "sandbox_api_key": "foobar"
-  },
-
-  "amazon": {
-      "access_key": "foobar",
-      "secret_access_key": "foobar",
-      "s3_bucket": "foobar"
-  },
-
-  "twilio": {
-    "sid": "foobar",
-    "token": "foobar"
   },
 
   "db": "mongodb://localhost/backpack-dev",
@@ -80,4 +38,4 @@ module.exports = {
     "service_password": "fooPassword"
   }
 
-}
+};
