f664b42a58843a5a4cbcec02fa311a570933bb74
CJ Ponti <cjponti@gmail.com>
2015-01-24 09:20:08 +0000

    Wrap client Socket.io in if (window.io)

--- client/main.jsx
+++ client/main.jsx
@@ -1,11 +1,13 @@
 'use strict';
 
-// Initialise Socket.io
-var socket = io.connect('http://localhost:3000');
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
-});
+if (window.io) {
+  // Initialise Socket.io
+  var socket = io.connect('http://localhost:3000');
+  socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
+}
 
 // Get initial data
 require('./app').getInitialData();
