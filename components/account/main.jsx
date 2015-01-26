2ac3ca20321a341ce46b7b9518fc443fedc1b633
CJ Ponti <cjponti@gmail.com>
2015-01-26 17:04:59 +0000

    Refactor account/main.jsx

--- components/account/main.jsx
+++ components/account/main.jsx
@@ -6,7 +6,6 @@ var AppConstants = App.constants;
 var AppDispatcher = App.dispatcher;
 var ConnectedAccount = require('./connected-account.jsx');
 var React = require('react');
-var UserStore = require('../../client/stores/users');
 
 var Account = React.createClass({
 
@@ -19,13 +18,16 @@ var Account = React.createClass({
    * Get initial state of component
    */
   getInitialState: function() {
-    return { user: {}, accounts: [] };
+    return {
+      user: App.user,
+      accounts: App.user.getAccounts()
+    };
   },
 
   /**
-   * Set state of component when UserStore is loaded
+   * Update state of component when User is changed
    */
-  onUserStoreChange: function(user) {
+  onUserChange: function(user) {
     return this.setState({
       user: user,
       accounts: UserStore.getUserAccounts()
@@ -33,23 +35,11 @@ var Account = React.createClass({
   },
 
   /**
-   * Perform logic when component is about to mount to
-   * the DOM
-   */
-  componentWillMount: function() {
-    if (UserStore._user) {
-      return this.onUserStoreChange(UserStore._user);
-    }
-
-    UserStore.on('change', this.onUserStoreChange);
-    return UserStore.fetch();
-  },
-
-  /**
    * Render component
    * @return {ReactCompositeComponent}
    */
   render: function() {
+    var user = this.state.user;
     return (
       <div className="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1 className="text-center"> Account </h1>
@@ -57,11 +47,11 @@ var Account = React.createClass({
           <li className="list-group-item">
             <div className="account-details text-center">
               <p>
-                <b>Email:</b> {this.state.user.email}
+                <b>Email:</b> {user.get('email')}
               </p>
             </div>
           </li>
-          {_.map(this.state.accounts, function(profile, provider) {
+          {_.map(user.getAccounts(), function(profile, provider) {
             return <ConnectedAccount provider={provider} profile={profile} />;
           })}
         </ul>
