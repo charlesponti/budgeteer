a48b9e4547ed45a4ddf818448ccf50ca50c33602
CJ Ponti <cjponti@gmail.com>
2015-01-26 17:05:21 +0000

    Move router.jsx to app/

---
+++ client/app/router.jsx
@@ -0,0 +1,62 @@
+'use strict';
+
+var Account = require('../../components/account/main.jsx');
+var Backbone = require('backbone');
+var React = require('react');
+var TaskApp = require('../../components/task/main.jsx');
+var Weight = require('../../components/weight/main.jsx');
+
+module.exports = function(app) {
+
+  var Router = Backbone.Router.extend({
+
+    routes: {
+      'account': 'account',
+      'weight': 'weight',
+      'tasks': 'tasks',
+      '*default': 'home'
+    },
+
+    home: function() {
+      return;
+    },
+
+    // Render Tasks application
+    tasks: function() {
+      if (App.authenticated) {
+        return React.render(<TaskApp />, App.el);
+      }
+      this.navigate('/')
+    },
+
+    // Render Weight application
+    weight: function() {
+      if (App.authenticated) {
+        return React.render(<Weight />, App.el);
+      }
+      this.navigate('/');
+    },
+
+    // Render Account application
+    account: function() {
+      if (App.authenticated) {
+        return React.render(<Account />, App.el);
+      }
+      this.navigate('/');
+    },
+
+    dispatcherIndex: function(payload) {
+      if (payload.navigate) {
+        this.navigate(payload.navigate, true);
+      }
+    }
+
+  });
+
+  var AppRouter = new Router();
+
+  app.dispatcher.register(AppRouter.dispatcherIndex.bind(AppRouter));
+
+  Backbone.history.start({ pushState: true });
+
+}
