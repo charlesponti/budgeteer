b3084a4e277558576c3995198c5689bfe640490a
hackefeller <hackefeller@icloud.com>
2015-11-03 17:02:00 +0000

    Add logic to users/login.js

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -1,1 +1,19 @@
 'use strict';
+
+module.exports = function(router) {
+  return router.post('/', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    Parse.User.logIn(username, password, {
+      success: function(user) {
+        console.log(user);
+        req.session.token = user._sessionToken;
+        res.sendStatus(200);
+      },
+      error: function(user, error) {
+        res.status(400).send(error);
+      }
+    });
+  });
+};
