e818934328349c91f64cec8996f7bac5af70cead
hackefeller <hackefeller@icloud.com>
2015-11-08 17:03:29 +0000

    Update config files

--- config/environments/test.js
+++ config/environments/test.js
@@ -1,41 +1,26 @@
 /**
- * @desc Configuration for applicatio
+ * @desc Configuration for application
  * @type {Object}
  */
 module.exports = {
-
-  "port": 3000,
-
-  "appName": "Backpack",
-
-  "appKey": "cthulhurules",
-
-  "session_ttl": 360000,
-
-  "sessionSecret": "cthulhurules",
-
-  "sessionStore": "cthulhu-session",
-
-  "static": "../static",
-
-  "views": "../views",
-
-  "baseUrl": "http://127.0.0.1:3000",
-
-  "google": {
-    "realm": "http://127.0.0.1:3000",
-    "client_id": process.env.GOOGLE_ID,
-    "client_secret": process.env.GOOGLE_SECRET,
-    "redirect_uri": "http://127.0.0.1:3000/auth/google/callback"
+  port: 3000,
+  appName: "Backpack",
+  appKey: "cthulhurules",
+  session_ttl: 360000,
+  sessionSecret: "cthulhurules",
+  sessionStore: "cthulhu-session",
+  baseUrl: "http://127.0.0.1:3000",
+  google: {
+    realm: "http://127.0.0.1:3000",
+    client_id: process.env.GOOGLE_ID,
+    client_secret: process.env.GOOGLE_SECRET,
+    redirect_uri: "http://127.0.0.1:3000/auth/google/callback"
   },
-
-  "db": "mongodb://localhost/backpack-dev",
-
-  "mailer": {
-    "service": "foobar",
-    "from_email": "foo@foo.com",
-    "service_username": "foo@foo.com",
-    "service_password": "fooPassword"
+  db: "mongodb://localhost/backpack-test",
+  mailer: {
+    service: "foobar",
+    from_email: "foo@foo.com",
+    service_username: "foo@foo.com",
+    service_password: "fooPassword"
   }
-
 };
