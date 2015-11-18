438a00fa371d9a7a58942197ba3f591de74f0f24
hackefeller <hackefeller@icloud.com>
2015-11-18 18:16:03 +0000

    Update User schema

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,7 +3,8 @@
 var mongoose = require('mongoose');
 
 var UserSchema = mongoose.Schema({
-  facebookId: {type: String, required: true}
+  id: {type: String, required: true},
+  accessToken: {type: String, required: true}
 });
 
 module.exports = mongoose.model('User', UserSchema);
