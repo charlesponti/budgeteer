e2b0f296057c8e323a42e2f19110bc6e6af6f8b2
hackefeller <hackefeller@icloud.com>
2015-12-02 18:22:19 +0000

    Add additional fields to user model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -4,6 +4,9 @@ var mongoose = require('mongoose');
 
 var UserSchema = mongoose.Schema({
   id: {type: String, required: true},
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  email: {type: String, required: true},
   accessToken: {type: String, required: true}
 });
 
