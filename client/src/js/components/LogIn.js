523e0bee84105bc922e0185b4f3012fb9e578ce3
hackefeller <hackefeller@icloud.com>
2015-12-02 18:24:37 +0000

    Change LogIn button to link to /auth/facebook

--- client/src/js/components/LogIn.js
+++ client/src/js/components/LogIn.js
@@ -1,22 +1,18 @@
 import auth from '../app/auth.js';
 
 export default React.createClass({
-  onButtonClick: function onFacebookButtonClick() {
-    auth.login();
-  },
-
   render: function onLogInRender() {
     const buttonStyle = {
       backgroundColor: '#1E3F5D',
-      border: 'none',
       padding: '5px 17px',
       borderRadius: '3px',
       color: 'white'
     };
 
     return (
-      <button style={buttonStyle}
-              onClick={this.onButtonClick}>Log In With Facebook</button>
+      <a href="/auth/facebook" style={buttonStyle}>
+        Log In With Facebook
+      </a>
     );
   }
 });
