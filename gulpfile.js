94357823592878b3bad425c5e4693822081d9679
Charles Ponti <cj@ponti.io>
2016-01-31 12:41:04 +0000

    Remove gulp tasks for js and css as these will be handled by webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 'use strict';
 
 // Dependencies
-var config = require('./gulp/config');
 var buffer = require('vinyl-buffer');
 var gulp = require('gulp');
 var psi = require('psi');
 var runSequence = require('run-sequence');
 var $ = require('gulp-load-plugins')();
 var watchify = require('watchify');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var gulpif = require('gulp-if');
+var gutil = require('gulp-util');
+var notify = require('gulp-notify');
+var source = require('vinyl-source-stream');
+var streamify = require('gulp-streamify');
+var uglify = require('gulp-uglify');
 
 /**
  * If gulp tasks should be run in 'production' mode
@@ -37,45 +44,4 @@ gulp.task('pagespeed:desktop', function(done) {
 // Run Pagespeed Insights on both mobile and desktop
 gulp.task('pagespeed', function() {
   return runSequence('pagespeed:mobile', 'pagespeed:desktop');
-});
-
-// Lint and build source JavaScript files
-gulp.task('js', require('./gulp/browserify'));
-
-// Build CSS files
-gulp.task('css', function() {
-  var stream = gulp.src(config.css.main);
-
-  stream
-    .pipe(!isProd ? $.sourcemaps.init({ loadMaps: true }) : $.util.noop())
-    .pipe($.less())
-    .pipe(!isProd ? $.sourcemaps.write() : $.util.noop())
-    .pipe($.size({ title: 'CSS' }))
-    .pipe(gulp.dest(config.css.dest));
-
-  return stream;
-});
-
-// Optimize Images
-gulp.task('images', function() {
-  return gulp.src(['src/images/**/*'])
-    .pipe(gulp.dest('dist/images'));
-});
-
-
-gulp.task('watch', function() {
-  gulp.watch(config.css.source, ['css']);
-});
-
-// Build development assets
-gulp.task('build', function() {
-  return runSequence(['images'], ['js', 'css' ]);
-});
-
-// Build development assets and serve
-gulp.task('default', function() {
-  if (isWatching) {
-    return runSequence(['build'], 'watch');
-  }
-  return runSequence('build');
 });
