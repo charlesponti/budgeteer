b8d17eac10c4a470ac4ec900958facd66afa71d4
Charles Ponti <cj@ponti.io>
2016-02-13 11:47:59 +0000

    Remove unused variable from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,8 @@
 'use strict';
 
 // Dependencies
-var buffer = require('vinyl-buffer');
 var gulp = require('gulp');
 var psi = require('psi');
-var runSequence = require('run-sequence');
-var $ = require('gulp-load-plugins')();
-var watchify = require('watchify');
-var babelify = require('babelify');
-var browserify = require('browserify');
-var gulpif = require('gulp-if');
-var gutil = require('gulp-util');
-var notify = require('gulp-notify');
-var source = require('vinyl-source-stream');
-var streamify = require('gulp-streamify');
-var uglify = require('gulp-uglify');
-
-/**
- * If gulp tasks should be run in 'production' mode
- * @return {Boolean}
- */
-var isProd = $.util.env.prod;
-var isWatching = $.util.env.watch;
-
-var bower = function(path) { return 'bower_components/'+path; };
 
 // Run Pagespeed Insights on mobile
 gulp.task('pagespeed:mobile', function(done) {
