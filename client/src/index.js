c507597282ea39bc9a4fa51a11b074cf9bb12bcc
Charles Ponti <cj@ponti.io>
2016-02-13 17:36:24 +0000

    Create <item-action-buttons> component

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import uirouter from 'angular-ui-router';
 //import routes from './app/routes';
 import navigation from './site-navigation.html';
 import costPerDay from './components/CostPerDay';
+import itemActionButtons from './item-action-buttons';
 
 // Import bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,12 +12,11 @@ import 'bootstrap/dist/js/bootstrap';
 import './main.less';
 
 angular.module('backpack', [uirouter, costPerDay.name])
-  .directive('navigation', () => {
-    return {
-      restrict: 'E',
-      template: navigation
-    }
+  .component('navigation', {
+    restrict: 'E',
+    template: navigation
   })
+  .component('itemActionButtons', itemActionButtons)
   .config(['$urlRouterProvider', function($urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
   }]);
---
+++ client/src/item-action-buttons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import template from './item-action-buttons.html';
+
+export default {
+  restrist: 'E',
+  bindings: {edit: '&', remove: '&'},
+  template,
+  controller: function () {
+
+  }
+};
