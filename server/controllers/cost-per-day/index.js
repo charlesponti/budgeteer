16b7dfe083f3f215bbec3007cbc912a4029eef2b
Charles Ponti <cj@ponti.io>
2016-02-14 18:01:13 +0000

    Create cost-per-day controller

---
+++ server/controllers/cost-per-day/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var CostPerDay = require('mongoose').model('CostPerDay');
+
+module.exports = function(router) {
+  router.get('/', function(req, res, next) {
+    CostPerDay.find().exec(function(err, items) {
+      if (err) {
+        return next(err);
+      }
+
+      return res.json({items: items});
+    })
+  });
+
+  router.post('/', function(req, res, next) {
+    var weight = new CostPerDay(req.body);
+    var id = weight.get('id');
+
+    CostPerDay.findOne({id: id}).exec(function(err, doc) {
+      if (err) {
+        return next(err);
+      }
+
+      if (!doc) {
+        return weight.save(function(err, weight) {
+          if (err) {
+            return next(err);
+          }
+
+          return res.json({weight: weight});
+        });
+      }
+      else {
+        return res.status(409).json({message: 'weight already exists', weight: doc});
+      }
+    });
+  });
+
+  router.delete('/', function(req, res, next) {
+    CostPerDay.remove({ _id: req.query.id }, function(err) {
+      if (err) {
+        return next(err);
+      }
+
+      return res.json({message: 'item deleted'});
+    });
+  });
+};
