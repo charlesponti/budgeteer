71f3155fad954f3f94cdb1872755653ad885876f
Charles Ponti <cj@ponti.io>
2016-02-17 19:45:56 +0000

    Use import statements in src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,28 +2,30 @@ const angular = require('angular')
 const uirouter = require('angular-ui-router')
 require('angular-resource')
 
-const navigation  = require('./site-navigation.html')
-const costPerDay = require('./components/CostPerDay')
-const weights = require('./components/Weight')
-const itemActionButtons = require('./components/item-action-buttons')
+const navigation = require('./site-navigation.html')
+import possessions from './possessions'
+import weight from './weight'
+import transactions from './transactions'
+import itemActionButtons from './components/item-action-buttons'
 
 // Import bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
 
-import './main.less';
+import './main.less'
 
 angular.module('backpack',
   [
     uirouter,
-    costPerDay.name,
-    weights.name
+    possessions.name,
+    weight.name,
+    transactions.name
   ])
   .component('navigation', {
     restrict: 'E',
     template: navigation
   })
   .component('itemActionButtons', itemActionButtons)
-  .config(['$urlRouterProvider', function($urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
-  }]);
+  .config(['$urlRouterProvider', function ($urlRouterProvider) {
+    $urlRouterProvider.otherwise('/')
+  }])
