03127a02b010128aa74709243e275e6a4e9b074d
Charles Ponti <cj@ponti.io>
2016-02-23 11:00:30 +0000

    Add transactions component

--- client/src/transactions/index.js
+++ client/src/transactions/index.js
@@ -6,6 +6,13 @@ export default (
   angular
     .module('transactions', [uirouter])
     .component('transactionListItem', transactionListItem)
+    .component('transactions', {
+      restrict: 'E',
+      template,
+      controller: ['Transaction', function (Transaction) {
+        this.transactions = Transaction.query()
+      }]
+    })
     .config(['$stateProvider', function ($stateProvider) {
       $stateProvider
         .state('transactions', {
