4bb0566650512202da5d3a6f266636433fe0f39e
Charles Ponti <cj@ponti.io>
2016-02-24 16:38:55 +0000

    Add account model

---
+++ server/models/account.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import mongoose from 'mongoose'
+
+const Account = new mongoose.Schema({
+  name: String,
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+})
+
+module.exports = mongoose.model('Account', Account)
