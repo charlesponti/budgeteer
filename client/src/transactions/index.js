2fb6b99810f181b0de0aebb0715ac4e5754015b1
Charles Ponti <cj@ponti.io>
2016-02-24 15:10:30 +0000

    Check $stateParams for .record in Transaction form

--- client/src/transactions/index.js
+++ client/src/transactions/index.js
@@ -28,7 +28,13 @@ export default (
     .component('transactionForm', {
       restirct: 'E',
       template: require('./transaction-form.html'),
-      controller: ['Transaction', '$state', function (Transaction, $state) {
+      controller: ['Transaction', '$state', '$stateParams', function (Transaction, $state, $stateParams) {
+        if (angular.isObject($stateParams.record)) {
+          this.record = $stateParams.record
+        } else {
+          this.record = {}
+        }
+
         this.onSubmit = function () {
           Transaction.save(this.record, (data) => {
             $state.go('transactions')
