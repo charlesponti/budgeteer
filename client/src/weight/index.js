7bd278c3defe483be95b77862bec7a692d561d25
Charles Ponti <cj@ponti.io>
2016-02-27 15:56:36 +0000

    Fix indentatino

--- client/src/weight/index.js
+++ client/src/weight/index.js
@@ -7,41 +7,44 @@ import WeightForm from './weight-form'
 
 export default (
   angular
-  .module('backpack.weights', [uirouter, 'ngResource'])
-  .factory('Weight', ['$resource', function ($resource) {
-    return $resource('/weights', {}, {
-      get: {
-        isArray: true,
-        transformResponse (data, headers) {
-          return angular.fromJson(data).map((weight) => {
-            weight.date = new Date(weight.date)
-            return weight
-          })
+    .module('backpack.weights', [uirouter, 'ngResource'])
+    .factory('Weight', ['$resource', function ($resource) {
+      return $resource('/weights', {}, {
+        get: {
+          isArray: true,
+          transformResponse (data, headers) {
+            return angular.fromJson(data).map((weight) => {
+              weight.date = new Date(weight.date)
+              return weight
+            })
+          }
         }
-      }
-    })
-  }])
-  .component('weights', {
-    restrict: 'E',
-    template: weightList,
-    controller: ['Weight', function (Weight) {
-      Weight.get({}, (data) => {
-        this.weights = data
       })
-    }]
-  })
-  .component('weightGraph', WeightGraph)
-  .component('weightTable', WeightTable)
-  .component('weightForm', WeightForm)
-  .config(['$stateProvider', function ($stateProvider) {
-    $stateProvider
-    .state('weights', {
-      url: '/weights',
-      template: '<weights></weights>'
-    })
-    .state('new-weight', {
-      url: '/weights/new',
-      template: '<weight-form></weight-form>'
-    })
-  }])
+    }])
+    .component('weightGraph', WeightGraph)
+    .component('weightTable', WeightTable)
+    .component('weightForm', WeightForm)
+    .config(['$stateProvider', function ($stateProvider) {
+      $stateProvider
+        .state('weights', {
+          url: '/weights',
+          template: weightList,
+          resolve: {
+            weights: function (Weight, $q) {
+              const defer = $q.defer()
+
+              Weight.query({}, (data) => defer.resolve(data))
+
+              return defer.promise
+            }
+          },
+          controller: ['weights', '$scope', function (weights, $scope) {
+            $scope.weights = weights
+          }]
+        })
+        .state('new-weight', {
+          url: '/weights/new',
+          template: '<weight-form></weight-form>'
+        })
+    }])
 )
