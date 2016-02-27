8d190051937d2888005a10e9bdb44b2b252982de
Charles Ponti <cj@ponti.io>
2016-02-27 15:58:41 +0000

    Add put for editing transactions

--- server/controllers/transactions/index.js
+++ server/controllers/transactions/index.js
@@ -13,6 +13,28 @@ module.exports = function (router) {
     })
   })
 
+  router.put('/', function (req, res, next) {
+    Transaction.findOne({ _id: req.body._id }).exec(function (err, doc) {
+      if (err) {
+        return next(err)
+      }
+
+      if (typeof doc === 'undefined') {
+        return res.status(400).json({ message: 'record '+req.query._id+' does not exist'})
+      }
+
+      if (doc) {
+        doc.update(req.body, function (err, doc) {
+          if (err) {
+            return next(err)
+          }
+
+          return res.status(200).json({ message: 'record updated', doc: doc })
+        })
+      }
+    })
+  })
+
   router.post('/', function (req, res, next) {
     var record = new Transaction(req.body)
 
