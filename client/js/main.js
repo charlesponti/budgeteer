d261f56faa8b42851136c15353bc27e2eb369981
charlesponti <cj@ponti.io>
2018-08-11 11:05:09 +0100

    WIP: Add oclif

--- client/js/main.js
+++ client/js/main.js
@@ -1,71 +1,72 @@
-$.get('/api', (data) => {
-  window.accounts = {}
-  var i = 0
-  var getAccounts = () => Object.keys(window.accounts)
+fetch('/api')
+  .then((data) => {
+    window.accounts = {}
+    var i = 0
+    var getAccounts = () => Object.keys(window.accounts)
 
-  // Ensure that transactions are sorted by date
-  data = _.sortBy(data, (d) => d.Date)
+    // Ensure that transactions are sorted by date
+    data = _.sortBy(data, (d) => d.Date)
 
-  data.forEach((transaction) => {
-    var account = transaction.Account
-    var date = transaction['Date']
-    var debit = transaction['Debit Amount']
-    var credit = transaction['Credit Amount']
+    data.forEach((transaction) => {
+      var account = transaction.Account
+      var date = transaction['Date']
+      var debit = transaction['Debit Amount']
+      var credit = transaction['Credit Amount']
 
-    if (getDays().indexOf(date) === -1) {
-      window.days[date] = { netWorth: 0, transactions: [] }
+      if (getDays().indexOf(date) === -1) {
+        window.days[date] = { netWorth: 0, transactions: [] }
+      }
+
+      if (getAccounts().indexOf(account) === -1) {
+        window.accounts[account] = { credit: 0, debit: 0, transactions: [] }
+      }
+
+      if (typeof debit === 'number') {
+        window.accounts[account].debit += debit
+        window.days[date].netWorth -= debit
+      }
+
+      if (typeof transaction['Credit Amount'] === 'number') {
+        window.accounts[account].credit += credit
+        window.days[date].netWorth += credit
+      }
+
+      transaction._id = i
+
+      i++
+
+      transaction.date = new Date(transaction.Date)
+
+      window.accounts[account].transactions.push(transaction)
+
+      db.put(transaction)
+    })
+
+    var accounts = Object.keys(window.accounts)
+
+    var netWorth = 0
+
+    function moneyFormat(number) {
+      return numeral(number).format('0,0.00')
     }
 
-    if (getAccounts().indexOf(account) === -1) {
-      window.accounts[account] = {credit: 0, debit: 0, transactions: []}
-    }
+    accounts.forEach((account) => {
+      var accountName = account + ''
+      account = window.accounts[account]
+      account.balance = (account.credit - account.debit).toFixed(2)
+      netWorth += parseFloat(account.balance)
 
-    if (typeof debit === 'number') {
-      window.accounts[account].debit += debit
-      window.days[date].netWorth -= debit
-    }
+      var accountEl = $(`
+        <div class="col-md-4">
+            <h2>${accountName}</h2>
+            <h3>Balance: £ ${moneyFormat(account.balance)}</h3>
+        </div>
+      `)
 
-    if (typeof transaction['Credit Amount'] === 'number') {
-      window.accounts[account].credit += credit
-      window.days[date].netWorth += credit
-    }
+      $('.container .row').append(accountEl)
+    })
 
-    transaction._id = i
+    $('#net-worth').text('Net Worth: £ ' + moneyFormat(netWorth.toFixed(2)))
 
-    i++
-
-    transaction.date = new Date(transaction.Date)
-
-    window.accounts[account].transactions.push(transaction)
-
-    db.put(transaction)
+    console.log(window.accounts)
   })
-
-  var accounts = Object.keys(window.accounts)
-
-  var netWorth = 0
-
-  function moneyFormat(number) {
-    return numeral(number).format('0,0.00')
-  }
-
-  accounts.forEach((account) => {
-    var accountName = account + ''
-    account = window.accounts[account]
-    account.balance = (account.credit - account.debit).toFixed(2)
-    netWorth += parseFloat(account.balance)
-
-    var accountEl = $(`
-      <div class="col-md-4">
-          <h2>${accountName}</h2>
-          <h3>Balance: £ ${moneyFormat(account.balance)}</h3>
-       </div>
-    `)
-
-    $('.container .row').append(accountEl)
-  })
-
-  $('#net-worth').text('Net Worth: £ ' + moneyFormat(netWorth.toFixed(2)))
-
-  console.log(window.accounts)
-})
