08b554e3c0bf5627bd1d83ce787bb0f3d3b67aed
Charlie <cj@ponti.io>
2020-02-27 07:38:46 -0500

    wip

---
+++ services/web/project/main/controller/habit-tracker.py
@@ -0,0 +1,15 @@
+from typing import NamedTuple, List
+
+frequencies: List[NamedTuple] = [
+    ("EVERY_DAY", "some_value"),
+    ("EVERY_YEAR", "some_value"),
+]
+
+
+class Habit:
+    action: str
+    frequency: str
+
+
+casts = "foo"
+habits: List[Habit] = [{"action": "read 60 pages", "frequency": "EVERY_DAY"}]
